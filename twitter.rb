9a893dec108a547545d655ac97c155c7effe28e1
kkiiji <kkiiji@gmail.com>
2016-02-13 22:23:34 -0800

    add emotion

---
+++ twitter.rb
@@ -0,0 +1,14 @@
+require "twitter"
+require "byebug"
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "yxo0JF3jA2b9wAXw7FlTh7AbL"
+  config.consumer_secret     = "WELGTE2uzntcXCCgLpIiT4cwiQUwcWpUonf1fP90tteMQhQYKK"
+  config.access_token        = "3399344712-zazXzOhLHxIcspwZoIKwLJCL86QerEvThpuEexk"
+  config.access_token_secret = "ZV5bSIcfu0qLoLNcefXMaI8HUSRt88yhbpYMuhlAOOE7b"
+end
+
+timeline = client.user_timeline("realDonaldTrump")
+timeline.each do |tweet|
+  p tweet.text
+end
