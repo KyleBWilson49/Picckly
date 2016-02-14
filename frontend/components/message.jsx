c10a115651976fd371319440359c8774e340acbd
Christopher Umbach <chrisumbach@gmail.com>
2016-02-13 23:04:35 -0800

    api calls and updating

--- frontend/components/message.jsx
+++ frontend/components/message.jsx
@@ -12,9 +12,30 @@ var Message = React.createClass({
   },
   handleChange: function(e) {
     if (e.target.value[e.target.value.length-1] === " ") {
-      
+      this.apiCall(e.target.value);
     }
     this.setState({ inputVal: e.target.value })
+  },
+  apiCall: function(messageText) {
+    var that = this;
+    var acctkey = window.btoa("AccountKey:eATIdDoYXwTq/ig6ZMB/sAz0lmiP9oL7DzDS6PExI4A");
+    $.ajax({
+      url: "https://api.datamarket.azure.com/data.ashx/amla/text-analytics/v1/GetSentiment?",
+      beforeSend: function(xhrObj){
+          xhrObj.setRequestHeader("Accept","application/json");
+          xhrObj.setRequestHeader("Authorization","Basic " + acctkey);
+      },
+      type: "GET",
+      data: {
+        Text:messageText
+      }
+    })
+    .done(function(data) {
+        that.setState({ moodVal: data.Score})
+    })
+    .fail(function() {
+        alert("error");
+    });
   },
 
   // componentDidMount: function(){
--- frontend/picckly.jsx
+++ frontend/picckly.jsx
@@ -5,13 +5,10 @@ var Route = require('react-router').Route;
 var IndexRoute = require('react-router').IndexRoute;
 var FrontPage = require('./components/front_page.jsx');
 var Graphs = require('./components/graphs.jsx');
-<<<<<<< HEAD
 var Message = require('./components/message.jsx');
-=======
 var MoodRing;
 var TextMood;
 var TwitterMood;
->>>>>>> 31660a811f904de8698361219265db6e5303c757
 
 var App = React.createClass({
   render: function () {
@@ -24,18 +21,12 @@ var App = React.createClass({
 var routes = (
   <Route path="/" component={App}>
     <IndexRoute component={FrontPage}/>
-<<<<<<< HEAD
-=======
     <Route path="/graphs" component={Graphs}/>
-<<<<<<< HEAD
     <Route path="/message" component={Message}/>
-=======
->>>>>>> 5e5c70255e2b9f7e65d675c935228196ea35fe5a
     <Route path="/moodring" component={MoodRing}/>
     <Route path="/graphs" component={Graphs}/>
     <Route path="/textmood" component={TextMood}/>
     <Route path="/twittermood" component={TwitterMood}/>
->>>>>>> 31660a811f904de8698361219265db6e5303c757
   </Route>
 );
 
