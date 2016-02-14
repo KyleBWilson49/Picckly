335163dfb19b451a1ecf48e3966848a79722ed53
Ryan Samp <sampryan@gmail.com>
2016-02-14 08:41:08 -0800

    Style updates

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -47,33 +47,18 @@ var Navbar = React.createClass({
                                 onClick={this.graphsTab}>Graphs</span> :
                           <span className="graphs icon-bar navbar-tab"
                                 onClick={this.graphsTab}>Graphs</span>;
-<<<<<<< HEAD
     var text = curr === "text" ?
                           <span className="text active icon-bar navbar-tab"
                                 onClick={this.textTab}>Text Mood</span> :
                           <span className="text icon-bar navbar-tab"
-=======
-    var text = curr === "message" ?
-                          <span className="navbar-tab message active"
-                                onClick={this.textTab}>Text Mood</span> :
-                          <span className="navbar-tab message"
->>>>>>> 44b7b7057e8e7c1fa0681bad89200785760c77a7
                                 onClick={this.textTab}>Text Mood</span>;
     var twitter = curr === "twitter" ?
                           <span className="twitter active icon-bar navbar-tab"
                                 onClick={this.twitterTab}>Twitter</span> :
                           <span className="twitter icon-bar navbar-tab"
                                 onClick={this.twitterTab}>Twitter</span>;
-<<<<<<< HEAD
-    var logout = curr === "logout" ?
-                          <span className="logout active icon-bar navbar-tab last"
-                                onClick={this.logoutTab}>Log Out</span> :
-                          <span className="logout icon-bar navbar-tab last"
-                                onClick={this.logoutTab}>Log Out</span>;
-=======
-    var logout =  <span className="navbar-tab logout"
-                        onClick={this.logoutTab}>Log Out</span>
->>>>>>> 44b7b7057e8e7c1fa0681bad89200785760c77a7
+    var logout =  <span className="navbar-tab logout last icon-bar"
+                        onClick={this.logoutTab}>Log Out</span>;
 
     return(
       <navbar className="navbar navbar-default navbar-fixed-top">
