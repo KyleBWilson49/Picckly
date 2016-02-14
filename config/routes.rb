83090d6e72feaae1e17f832a292652aefbf4957b
KyleBWilson49 <KyleBWilson49@gmail.com>
2016-02-14 03:18:22 -0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,8 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
-<<<<<<< HEAD
-      resources :emotions, only: [:index]
-      resources :users, only: [:create]
-      resource :session, only: [:create, :destroy]
-=======
-    resources :emotions, only: [:index]
-    resources :tweets, only: [:create]
->>>>>>> 7ef2f9a047aa7c77bd86f44189cb8dee59bfeacb
-  end
+     resources :emotions, only: [:index]
+     resources :user, only: [:create]
+     resource :session, only: [:create, :destroy]
+   end 
 end
